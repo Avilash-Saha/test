d2a411296ec24109994bbfcf41e6b51a88a412a8
Avilash Saha <avilashs@ami.com>
2025-10-07 14:55:47 +0530

    meifoon

--- hungry.py
+++ hungry.py
@@ -1,6 +1,7 @@
 hungry = input("Are you hungry? (yes/no): ").strip().lower()
 if hungry == "yes":
     print("Eat Chilli Chicken!")
+    print("Eat Meifoon!")
 else:
     thrirsty = input("Are you thirsty? (yes/no): ").strip().lower()
     if thrirsty == "yes":
